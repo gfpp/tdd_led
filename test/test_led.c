d281e0afa69366041d03b9863ad715a1e6bb24d5
gfpp <gfperezpaina@gmail.com>
2017-08-14 14:15:24 -0300

    Test que falla por error de compilación (prueba función LedInit).

--- test/test_led.c
+++ test/test_led.c
@@ -11,6 +11,9 @@ void tearDown(void)
 
 void test_LedsOffAfterCreate(void)
 {
-  TEST_FAIL_MESSAGE("Empezamos");
+/*  TEST_FAIL_MESSAGE("Empezamos");*/
+  uint16_t ledPort = 0xFFFF;
+  LedInit(&ledPort);
+  TEST_ASSERT_EQUAL_HEX16(0x0000, ledPort);
 }
 
